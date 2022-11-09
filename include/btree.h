3017b30cb305226b45942425fad83ff465defd93
SirQuacc <steffentp@protonmail.ch>
2022-11-09 13:09:53 +0100

    save this

--- include/btree.h
+++ include/btree.h
@@ -1,16 +1,17 @@
 #ifndef BTREE_H
 #define BTREE_H
+#include <stdbool.h>
 
-struct tree_node {
+typedef struct tree_node { //Changed to typedef for easier coding
   int item;
   struct tree_node *left;
   struct tree_node *right;
-};
+} tree_node ;
 
 int Empty(struct tree_node *t);
 struct tree_node *Insert(int x, struct tree_node *t);
 struct tree_node *Remove(int x, struct tree_node *t);
-int Contains(int x, struct tree_node *t);
+bool Contains(int x, struct tree_node *t);
 struct tree_node *Initialize(struct tree_node *t);
 int Full(struct tree_node *t);
 
--- src/btree.c
+++ src/btree.c
@@ -6,9 +6,27 @@
 #include <stdlib.h>
 
 struct tree_node *Insert(int x, struct tree_node *t) {
-  // Insert item x into the tree t
+  if(Empty(t)) {
+    t = malloc(sizeof(tree_node));
+    t->item = x; t->left = NULL; t->right = NULL;
+    return t;
+  } 
+  if(x <= t->item && t->left != NULL){
+    t->left = Insert(x, t->left);
+  } else if(x <= t->item){
+    t->left = malloc(sizeof(tree_node));
+    t->left->item = x; t->left->left = NULL; t->left->right = NULL;
+    return t; //Maybe unnecessary
+  }
+  if(x > t->item && t->right != NULL){
+    t->right = Insert(x, t->right);
+  } else if(x > t->item){
+    t->right = malloc(sizeof(tree_node));
+    t->right->item = x; t->left->left = NULL; t->left->right = NULL;
+    return t; //Maybe unnecessary
+  }
 
-  return NULL;
+  return t;
 }
 
 struct tree_node *Remove(int x, struct tree_node *t) {
@@ -17,24 +35,23 @@ struct tree_node *Remove(int x, struct tree_node *t) {
   return NULL;
 }
 
-int Contains(int x, struct tree_node *t) {
-
+bool Contains(int x, struct tree_node *t) {
   // Return true if the tree t contains item x. Return false otherwise.
-
-  return NULL;
+  if(x == t->item) return true;
+  if(x < t->item) return Contains(x, t->left);
+  if(x > t->item) return Contains(x, t->right);
+  return false; //Base case
 }
 
 struct tree_node *Initialize(struct tree_node *t) {
-  // Create an empty tree
   return NULL;
 }
 
 int Empty(struct tree_node *t) {
-  // Test if empty
-  return 0;
+  return t == NULL;
 }
 
 int Full(struct tree_node *t) {
-  // Test if full
+  // Test if full (it can't be as a linked list)
   return 0;
 }
