2cdbd50c6a95d82ec01731c499b9dad2df7cbc73
SirQuacc <steffentp@protonmail.ch>
2022-11-09 16:50:28 +0100

    kinda works?

--- src/btree.c
+++ src/btree.c
@@ -13,17 +13,21 @@ struct tree_node *Insert(int x, struct tree_node *t) {
   } 
   if(x <= t->item && t->left != NULL){
     t->left = Insert(x, t->left);
-  } else if(x <= t->item){
+  } 
+  else { 
+    if(x <= t->item){
     t->left = malloc(sizeof(tree_node));
     t->left->item = x; t->left->left = NULL; t->left->right = NULL;
-    return t; //Maybe unnecessary
+    }
   }
   if(x > t->item && t->right != NULL){
     t->right = Insert(x, t->right);
-  } else if(x > t->item){
+  } 
+  else {
+    if(x > t->item){
     t->right = malloc(sizeof(tree_node));
-    t->right->item = x; t->left->left = NULL; t->left->right = NULL;
-    return t; //Maybe unnecessary
+    t->right->item = x; t->right->left = NULL; t->right->right = NULL;
+    }
   }
 
   return t;
@@ -31,15 +35,64 @@ struct tree_node *Insert(int x, struct tree_node *t) {
 
 struct tree_node *Remove(int x, struct tree_node *t) {
   // Remove one item from the tree t
+  tree_node* copy = t;
+  tree_node** tPoint = &t;
+  tree_node* deletDis;
+  tree_node** prevPoint;
+  while(!Empty(t)){
+    if(x == copy->item){ //We are at the node we want to remove
+      deletDis = copy; //Copy current node's pointer, to free up later
+      if(copy->left == NULL){ //If our node doesn't have any left child, we set the pointer to t, to point to t's right child instead.
+        *tPoint = copy->right;
+        free(deletDis); //Free up the node we want to remove
+        break; //Done, exit while loop and return tree
+      }
+      if(copy->right == NULL){ //Same as above, but with the right child instead of left
+        *tPoint = copy->left;
+        free(deletDis);
+        break;
+      }
+      if(copy->right != NULL && copy->left != NULL){ //If the node we want to free, has both children, it gets more complicated
+        prevPoint = &((*tPoint)->left); //Move left, and keep track of the pointer that points to this spot
+        copy = copy->left; //Move left
+        while(copy->right != NULL){ //Keep going right, to seek the largest number that is lower than the one we want to remove
+          copy = copy->right;
+          prevPoint = &((*prevPoint)->right);
+        }
+        if(copy->left != NULL){ //If this largest number has a left child, we need to remove this largest node, but reassign the pointer to it, to whatever it's left child was.
+          *prevPoint = copy->left;
+        }
 
-  return NULL;
+        *tPoint = copy; //Now, the pointer to whatever we wanted to remove, needs to instead point to this largest node, t
+        
+        if(copy->left != NULL){ //Might be unnecessary to have if/else
+          copy->right = deletDis->right;
+          copy->left = deletDis->left;
+        } else{
+          copy->right = deletDis->right;
+        }
+       
+      }
+      free(deletDis);
+      break;
+    }
+    if(x < copy->item){
+      copy = copy->left;
+      tPoint = &((*tPoint)->left);
+    } else {
+      copy = copy->right;
+      tPoint = &((*tPoint)->right);
+    }
+  }
+
+  return t;
 }
 
 bool Contains(int x, struct tree_node *t) {
   // Return true if the tree t contains item x. Return false otherwise.
   if(x == t->item) return true;
-  if(x < t->item) return Contains(x, t->left);
-  if(x > t->item) return Contains(x, t->right);
+  if(x < t->item && t->left != NULL) return Contains(x, t->left);
+  if(x > t->item && t->right != NULL) return Contains(x, t->right);
   return false; //Base case
 }
 
--- src/linked_list.c
+++ src/linked_list.c
@@ -26,20 +26,40 @@ void free_list(node *p) {
 void print_list(node *p) {
   // Add your code for exercise 1
   // There is NO testcode for this
+  
+  printf("-> %d ", p->value); //Print current value
+  if(p->next != NULL) print_list(p->next); //If we're not at the end, recursively send the next node
 }
 
 int sum_squares(node *p) {
   // Add your code for excercise 2
   // You can find the tests in tests.cpp
-  return -1;
+  if(p == NULL) //If we've gotten to the null pointer at the end of the list, or input was empty, return 0
+  {
+    return 0;
+  }
+  else{
+    return (p->value*p->value + sum_squares(p->next)); //square the input value and recursively add the same from the next nodes.
+  }
 }
 
 typedef int (*fn_int_to_int)(int);
 
 node *map(node *p, fn_int_to_int f) { 
   // Add your code for excercise 3
-  
-  return NULL; 
+  node* curNew = NULL; //Pointer that goes through the creation of a new list
+  node* head = NULL; //Pointer to save the head of that new list
+  if(p != NULL){ //Assuming we're not receiving an empty list, create first node and update head.
+    curNew = make_node(f(p->value), NULL);
+    head = curNew;
+    p = p->next; //Scroll onwards in given list
+  }
+  while(p != NULL){ //Until we reach the end of the list, create new nodes applying the given function and add them to the new list
+    curNew->next = make_node(f(p->value), NULL);
+    curNew = curNew->next;
+    p=p->next;
+  }
+  return head; //Return pointer to the head of the new list.
 }
 
 int square(int x) { return x * x; }
